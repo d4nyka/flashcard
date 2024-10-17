fab677bbbe7b2e053be9eba384365272fba0062b
d4nyka <danillesantod@gmail.com>
2024-10-17 11:05:30 -0300

    Add files via upload

---
+++ Assets/perguntas.js
@@ -0,0 +1,16 @@
+criaCartao(
+    'Taylor Swift',
+    'Quem é Taylor Swift?',
+    'Taylor Swift é cantora e compositora e uma das mulheres mais bem sucedidas do mundo da música.'
+)
+
+criaCartao(
+    'Taylor Swift',
+    'Qual foi o primeiro album da Taylor Swift?',
+    'O primeiro álbum da cantora, foi Taylor Swift, que foi lançando em 2006.'
+)
+
+criaCartao(
+    'Taylor Swift',
+    'Quantos grammys Taylor Swift tem?',
+)
